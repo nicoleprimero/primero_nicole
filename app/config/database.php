0c74caf65595cc1e94c0144aeb60aeaedeaeab52
Nicole Primero <nicoleprimero20@gmail.com>
2025-09-17 19:06:37 +0800

    Second commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'sofia',
+    'username'	=> 'sql12798331',
+    'password'	=> 'WJvnaDq7ii',
+    'database'	=> 'sql12798331',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
